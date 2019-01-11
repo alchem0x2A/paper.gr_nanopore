320f17253733f727c154a595528cee2add923d19
T.Tian <tian.tian@chem.ethz.ch>
2019-01-11 11:47:42 +0100

    Add new plots for fig6

--- paper/src/plot_rec_all.py
+++ paper/src/plot_rec_all.py
@@ -13,26 +13,39 @@ def read(name):
     data[:, 1] -= data[data[:, 0] == 0, 1]
     return data, err
 
-fig = plt.style.use("science")
-plt.figure(figsize=(2.8, 2.8), facecolor="w")
-plt.axvline(x=0, ls="--", color="#00ffee")
-plt.axhline(y=0, ls="--", color="#00ffee")
+def convert(eta, delta):
+    assert delta > 0
+    xi_inv = (1 - eta) / (delta * eta + 1)
+    return 1 - xi_inv
 
-for name in ["KCl", "NaCl", "LiCl",
-             "MgSO4", "K2SO4", "CaCl2",
-             "K3FeCN"]:
+if __name__ == "__main__":
+    fig = plt.style.use("science")
+    plt.figure(figsize=(2.8, 2.8), facecolor="w")
+    plt.axvline(x=0, ls="--", color="#00ffee")
+    plt.axhline(y=0, ls="--", color="#00ffee")
 
-    data, err = read(name)
-    plt.errorbar(data[:, 0], data[:, 1], yerr=err / 2,
-                 fmt="o-", alpha=0.5, label=name,
-                 markersize=4)
-    # plt.fill_between(data[:, 0], data[:, 1] - err / 2,
-                     # data[:, 1] + err / 2,
-                     # fmt="o",
-                     # alpha=0.5, label=name)
-plt.ylim(-0.1, 0.7)
-plt.xlim(-1.25, 1.25)
-plt.xlabel("$V_{\\mathrm{G}}$ (V)")
-plt.ylabel("Rectification")
-plt.legend()
-plt.savefig("../img/rect_all.svg".format(name))
+    delta0 = 2.2
+    for name in ["KCl", "NaCl", "LiCl",
+                 "MgSO4", "K2SO4", "CaCl2",
+                 "K3FeCN"]:
+        data, err = read(name)
+        v = data[:, 0]
+        eta = data[:, 1]
+        xi = convert(eta, delta=delta0)
+        err_lo = xi - convert(eta - err / 2, delta=delta0)
+        err_hi = convert(eta + err / 2, delta=delta0) - xi
+        plt.errorbar(data[:, 0], xi,
+                     yerr=(err_lo / 2, err_hi / 2),
+                     # yerr=err / 2,
+                     fmt="o-", alpha=0.5, label=name,
+                     markersize=4)
+        # plt.fill_between(data[:, 0], data[:, 1] - err / 2,
+                         # data[:, 1] + err / 2,
+                         # fmt="o",
+                         # alpha=0.5, label=name)
+    plt.ylim(-0.1, 0.9)
+    plt.xlim(-1.25, 1.25)
+    plt.xlabel("$V_{\\mathrm{G}}$ (V)")
+    plt.ylabel("Rectification")
+    plt.legend()
+    plt.savefig("../img/rect_all.svg".format(name))
